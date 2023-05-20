57836a537bf0f87309b0a2728e07920797422e15
Joao-Filh0 <joaoadmob2@gmail.com>
2023-05-19 22:58:28 -0300

    test DATA

---
+++ test/core/data/data_mock.dart
@@ -0,0 +1,45 @@
+final pokeDataMock = {
+  "id": 1,
+  "name": "Bulbasaur",
+  "types": [
+    {
+      "type": {"name": "Grass"}
+    },
+    {
+      "type": {"name": "Poison"}
+    },
+  ],
+  "abilities": [
+    {
+      "ability": {"name": "Overgrow"}
+    },
+    {
+      "ability": {"name": "Chlorophyll"}
+    },
+  ],
+  "sprites": {
+    "other": {
+      "official-artwork": {
+        "front_default": "image_url",
+      }
+    }
+  },
+  "weight": 69,
+  "height": 7,
+  "stats": [
+    {"base_stat": 4500},
+    {"base_stat": 4900},
+    {"base_stat": 4900},
+    {"base_stat": 6500},
+    {"base_stat": 6500},
+    {"base_stat": 4500},
+  ],
+};
+
+final pokeUrlsDataMock = {
+  "results": [
+    {"url": "url1"},
+    {"url": "url2"},
+    {"url": "url3"},
+  ],
+};
---
+++ test/core/data/models/poke_model/poke_model_test_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pokedex/core/data/models/poke_model/poke_model.dart';
+
+import '../../data_mock.dart';
+
+void main() {
+  test('[DATA] - PokeModel', () {
+    const expectedModel = PokeModel(
+      id: 1,
+      name: "Bulbasaur",
+      types: ["Grass", "Poison"],
+      abilities: ["Overgrow", "Chlorophyll"],
+      image: "image_url",
+      weight: 6.9,
+      height: 0.7,
+      hp: 45,
+      atk: 49,
+      def: 49,
+      satk: 65,
+      sdef: 65,
+      spd: 45,
+    );
+
+    final model = PokeModel.fromMap(pokeDataMock);
+
+    expect(model, expectedModel);
+  });
+}
